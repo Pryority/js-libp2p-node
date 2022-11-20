9ecaab77354b4ba1a2fbe507709a0a19ee9d632f
pryority <matthewapryor@gmail.com>
2022-11-20 14:34:32 -0500

    add main function to start, stop node

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import { createLibp2p } from "libp2p";
-import { tcp } from '@libp2p/tcp';
-import { noise } from '@chainsafe/libp2p-noise';
-import { mplex } from '@libp2p/mplex';
+import {createLibp2p} from 'libp2p';
+import {tcp} from '@libp2p/tcp';
+import {noise} from '@chainsafe/libp2p-noise';
+import {mplex} from '@libp2p/mplex';
 
-const node = await createLibp2p({
+const main = async () => {
+  const node = await createLibp2p({
+    addresses: {
+      // add a listen address (localhost)
+      // to accept TCP connections on a random port
+      listen: ['/ip4/127.0.0.1/tcp/0'],
+    },
     transports: [tcp()],
     connectionEncryption: [noise()],
-    streamMuxers: [mplex()]
-})
+    streamMuxers: [mplex()],
+  });
+
+  await node.start();
+  console.log(``);
+
+  // print out listening addresses
+  console.log('listening on addresses:');
+  node.getMultiaddrs().forEach((addr) => {
+    console.log(addr.toString());
+  });
+
+  // stop libp2p
+  await node.stop();
+  console.log(`libp2p has stopped`);
+};
+
+main().then().catch(console.error);
