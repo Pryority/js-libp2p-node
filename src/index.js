ac807d10e62c4541e444c0c6c01862cc8e77eaeb
pryority <matthewapryor@gmail.com>
2022-11-20 14:24:50 -0500

    add a stream multiplexing module to improve the effectiveness and efficiency of connections for the various protocols libp2p runs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { createLibp2p } from "libp2p";
 import { tcp } from '@libp2p/tcp';
 import { noise } from '@chainsafe/libp2p-noise';
+import { mplex } from '@libp2p/mplex';
 
 const node = await createLibp2p({
     transports: [tcp()],
-    connectionEncryption: [noise()]
+    connectionEncryption: [noise()],
+    streamMuxers: [mplex()]
 })
