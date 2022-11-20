662a3f8758827ffce9a04aa99baf9d78e51d0427
pryority <matthewapryor@gmail.com>
2022-11-20 14:54:11 -0500

    add condition to ping peer if a multiaddr was received, refactor stop method into es6 function

--- src/index.js
+++ src/index.js
@@ -3,31 +3,45 @@ import {createLibp2p} from 'libp2p';
 import {tcp} from '@libp2p/tcp';
 import {noise} from '@chainsafe/libp2p-noise';
 import {mplex} from '@libp2p/mplex';
+import {multiaddr} from 'multiaddr';
 
-const main = async () => {
-  const node = await createLibp2p({
-    addresses: {
-      // add a listen address (localhost)
-      // to accept TCP connections on a random port
-      listen: ['/ip4/127.0.0.1/tcp/0'],
-    },
-    transports: [tcp()],
-    connectionEncryption: [noise()],
-    streamMuxers: [mplex()],
-  });
 
-  await node.start();
-  console.log(``);
+const node = await createLibp2p({
+  addresses: {
+    // add a listen address (localhost)
+    // to accept TCP connections on a random port
+    listen: ['/ip4/127.0.0.1/tcp/0'],
+  },
+  transports: [tcp()],
+  connectionEncryption: [noise()],
+  streamMuxers: [mplex()],
+});
 
-  // print out listening addresses
-  console.log('listening on addresses:');
-  node.getMultiaddrs().forEach((addr) => {
-    console.log(addr.toString());
-  });
+await node.start();
+console.log(``);
 
+// print out listening addresses
+console.log('listening on addresses:');
+node.getMultiaddrs().forEach((addr) => {
+  console.log(addr.toString());
+});
+
+// ping peer if received multiaddr
+if (process.argv.length >= 3) {
+  const ma = multiaddr(process.argv[2]);
+  console.log(`🟡 P I N G I N G remote peer at ${process.argv[2]}`);
+  const latency = await node.ping(ma);
+  console.log(`🟢 P I N G E D ${process.argv[2]} in ${latency}ms`);
+} else {
+  console.log('🔴 NO REMOTE PEER ADDRESS GIVEN, SKIPPING PING');
+}
+
+const stop = async () => {
   // stop libp2p
   await node.stop();
   console.log(`libp2p has stopped`);
+  process.exit(0);
 };
 
-main().then().catch(console.error);
+process.on('SIGTERM', stop);
+process.on('SIGINT', stop);
